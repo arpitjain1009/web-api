d9fc7876203f19d4d3182d79ac06a7d1554ab3ed
unknown <arpit.jain@lntinfotech.com>
2020-08-17 20:40:42 +0530

    b

---
+++ WebApplication1/Controllers/CustomerMastersController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    public class CustomerMastersController : ApiController
+    {
+        private FinanceDbEntities db = new FinanceDbEntities();
+
+        //GET: api/CustomerMasters
+        public IQueryable<CustomerMaster> GetCustomerMasters()
+        {
+            return db.CustomerMasters;
+        }
+
+        // GET: api/CustomerMasters/5
+        //[ResponseType(typeof(CustomerMaster))]
+        [HttpGet]
+        public object GetCustomerMaster([FromUri] string username, string ans1, string ans2, string ans3)
+        {
+            var emps = db.proc_GetAllDetails(username, ans1, ans2, ans3);
+
+            return emps;
+
+        }
+
+        // PUT: api/CustomerMasters/5
+        [ResponseType(typeof(void))]
+        public object PutCustomerMaster([FromUri]int id, string password, CustomerMaster c)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            //if (id != CustomerMaster.)
+            //{
+            //    return badrequest();
+            //}
+
+            //db.Entry(customerMaster).State = EntityState.Modified;
+
+            var emp = db.proc_UpdatePassword(id, password);
+            db.SaveChanges();
+            //try
+            //{
+            //    db.SaveChanges();
+            //}
+            //catch (DbUpdateConcurrencyException)
+            //{
+            //    if (!CustomerMasterExists(id))
+            //    {
+            //        return NotFound();
+            //    }
+            //    else
+            //    {
+            //        throw;
+            //    }
+            //}
+
+            return emp;
+        }
+
+        // POST: api/CustomerMasters
+        [ResponseType(typeof(CustomerMaster))]
+        public IHttpActionResult PostCustomerMaster(CustomerMaster customerMaster)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.CustomerMasters.Add(customerMaster);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = customerMaster.id }, customerMaster);
+        }
+
+        // DELETE: api/CustomerMasters/5
+        [ResponseType(typeof(CustomerMaster))]
+        public IHttpActionResult DeleteCustomerMaster(int id)
+        {
+            CustomerMaster customerMaster = db.CustomerMasters.Find(id);
+            if (customerMaster == null)
+            {
+                return NotFound();
+            }
+
+            db.CustomerMasters.Remove(customerMaster);
+            db.SaveChanges();
+
+            return Ok(customerMaster);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CustomerMasterExists(int id)
+        {
+            return db.CustomerMasters.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ WebApplication1/Models/CustMeta.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    [MetadataType(typeof(MetaDataForCustomerMaster))]
+    public partial class CustomerMaster
+    {
+    }
+    [DataContract]
+    public class MetaDataForCustomerMaster
+    {
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string FirstName { get; set; }
+        [DataMember]
+        public string MiddleName { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public string Phone { get; set; }
+        [DataMember]
+        public string PhoneAlt { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+        [DataMember]
+        public string UserName { get; set; }
+        [DataMember]
+        public string Password { get; set; }
+        [DataMember]
+        public string Address { get; set; }
+        [DataMember]
+        public string Bank { get; set; }
+        [DataMember]
+        public string SavingsAccountNumber { get; set; }
+        [DataMember]
+        public string IFSCCode { get; set; }
+        [DataMember]
+        public string ques1 { get; set; }
+        [DataMember]
+        public string ans1 { get; set; }
+        [DataMember]
+        public string ques2 { get; set; }
+        [DataMember]
+        public string ans2 { get; set; }
+        [DataMember]
+        public string ques3 { get; set; }
+        [DataMember]
+        public string ans3 { get; set; }
+    }
+}
